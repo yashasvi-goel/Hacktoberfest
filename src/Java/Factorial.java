ff96f0e13910eb2a07952ed3994095e7127d3b40
gkallepalli <Goutham_Kallepalli@intuit.com>
2018-10-17 20:04:49 +0530

    Changes:
    1. Added myself as contributor

--- src/Java/Factorial.java
+++
@@ -1,9 +0,0 @@
-public class Factorial {
-    public int factorial(int n){
-        int result = 1;
-        for(i=1; i<=n; i++){
-            result = result*i;
-        }
-        return result;
-    }
-}
